7c06f9b07cf29fc86ac5253d9ef6cbaccc1584d9
Flex01a <91494774+Flex01a@users.noreply.github.com>
2022-02-26 16:12:50 +0300

    Update atacamino.php

--- src/atacamino.php
+++ src/atacamino.php
@@ -1,150 +1,33 @@
 <?php
 
-function atacsid($sid)
-{
 
-   if ($sid === ""){
-   echo "sid required!";
-   }
-else{
-    if ($api === ""){
-        echo "q2m lib has an error";
-    }
-    elseif ($api === "send message"){
-        
-        
-                                                         
-                                                
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function ndcauth($ndcuath){
+    
 }
 
-function TypeMessage($Type){
- 
+function send($type,$message,$ndcid,$chatid){
+    
+   
+    $arr = array(
+        
+        "type"=>$type,
+        "content"=>"$message",
+        "clientRefId"=>585718302,
+        "attachedObject"=>null,
+        "timestamp"=>time()*1000
+        
+        
+        
+    
+        
+);
+$jso = json_encode($arr);
 
+$sors = hash_hmac('sha1',"fbf98eb3a07a9042ee5593b10ce9f3286a69d4e2",$jso,true);
+echo base64_encode(hex2bin("32").$sors);
 
-function message($message){
-
-
-function ndcid($com){
-
-function chat($chat){
-
-
-$time = time()*1000;
-
-
-
-$url = "https://service.narvii.com/api/v1/$com/s/chat/thread/$chat/message";
+$url = "https://service.narvii.com/api/v1/$ndcid/s/chat/thread/$chatid/message";
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
@@ -152,13 +35,14 @@ curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $headers = array(
-  "NDCAUTH:$sid",
+ "NDCAUTH:$ndcuath",
 "NDCDEVICEID:3271883BA66CFFC6D8D50118C22A8CD78F61E09B6369890162712A89979426EDE144127A98BFAD4D2E",
+"NDC-MSG-SIG:$sors", 
 );
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
 $data = <<<DATA
-{"type":$Type,"content":"$message","clientRefId":912612485,"attachedObject":null,"timestamp":$time}
+$jso
 DATA;
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 
@@ -169,19 +53,6 @@ curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 $resp = curl_exec($curl);
 curl_close($curl);
 var_dump($resp);
-
-}
 }
 
-}
-}
-}
 
-        
-        
-        
-    }
-
-
-
-?>
